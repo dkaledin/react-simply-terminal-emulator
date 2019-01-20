c87623f4f7331e9f0983d714f9badc9f3774c37d
Dmitriy Kaledin <fenomin@gmail.com>
2019-01-20 09:07:19 +0200

    CSS for Example page

--- example/index.tsx
+++ example/index.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Terminal} from '../src/terminal';
 
+import './index.css';
+
 ReactDOM.render(
     <Terminal/>,
     document.getElementById('root') as HTMLElement,
